c2f8314373a5cb47fc20c2147a96ac8b7e6ab4cb
Slava Kirillov <slava@exabyte.io>
2015-12-08 01:29:59 +0300

    Added function for search field

--- exabyte_theme/js/base.js
+++ exabyte_theme/js/base.js
@@ -65,12 +65,37 @@ $(document).ready(function() {
                 }
             });
         }
-    })
+    });
+
+    /*
+     * Text Feild
+     */
+
+    //Add blue animated border and remove with condition when focus and blur
+    if($('.fg-line')[0]) {
+        $('body').on('focus', '.form-control', function(){
+            $(this).closest('.fg-line').addClass('fg-toggled');
+        })
+
+        $('body').on('blur', '.form-control', function(){
+            var p = $(this).closest('.form-group');
+            var i = p.find('.form-control').val();
+
+            if (p.hasClass('fg-float')) {
+                if (i.length == 0) {
+                    $(this).closest('.fg-line').removeClass('fg-toggled');
+                }
+            }
+            else {
+                $(this).closest('.fg-line').removeClass('fg-toggled');
+            }
+        });
+    }
 });
 
 
 $('body').scrollspy({
-    target: '#sidebar'
+    target: '.bs-sidebar'
 });
 
 /* Prevent disabled links from causing a page reload */
