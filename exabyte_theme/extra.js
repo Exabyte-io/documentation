210c953eae884919fc8571a64d9caf1179cdc9e3
Timur Bazhirov <timur.bazhirov@gmail.com>
2015-12-10 00:26:58 -0800

    Linted js source in extra.js

--- exabyte_theme/extra.js
+++ exabyte_theme/extra.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
      * Sidebar open/close logic
      * Open sidebar on cliking to #menu-trigger and close on click outside sidebar
      */
-    $('body').on('click', '#menu-trigger', function(e){
+    $('body').on('click', '#menu-trigger', function(e) {
         e.preventDefault();
         var x = '#sidebar';
 
@@ -13,7 +13,7 @@ $(document).ready(function() {
         $('body').toggleClass('modal-open');
 
         //Close opened sub-menus
-        $('.sub-menu.toggled').not('.active').each(function(){
+        $('.sub-menu.toggled').not('.active').each(function() {
             $(this).removeClass('toggled');
             $(this).find('ul').hide();
         });
@@ -26,9 +26,9 @@ $(document).ready(function() {
 
         //When clicking outside
         if ($('#header').hasClass('sidebar-toggled')) {
-            $(document).on('click', function (e) {
+            $(document).on('click', function(e) {
                 if (($(e.target).closest($elem).length === 0) && ($(e.target).closest($elem2).length === 0)) {
-                    setTimeout(function(){
+                    setTimeout(function() {
                         $('body').removeClass('modal-open');
                         $($elem).removeClass('toggled');
                         $('#header').removeClass('sidebar-toggled');
@@ -43,12 +43,12 @@ $(document).ready(function() {
      * Text field (used in search)
      * Add blue animated border and remove with condition on focus and blur
      */
-    if($('.fg-line')[0]) {
-        $('body').on('focus', '.form-control', function(){
+    if ($('.fg-line')[0]) {
+        $('body').on('focus', '.form-control', function() {
             $(this).closest('.fg-line').addClass('fg-toggled');
         })
 
-        $('body').on('blur', '.form-control', function(){
+        $('body').on('blur', '.form-control', function() {
             var p = $(this).closest('.form-group');
             var i = p.find('.form-control').val();
 
@@ -56,12 +56,9 @@ $(document).ready(function() {
                 if (i.length == 0) {
                     $(this).closest('.fg-line').removeClass('fg-toggled');
                 }
-            }
-            else {
+            } else {
                 $(this).closest('.fg-line').removeClass('fg-toggled');
             }
         });
     }
 });
-
-
