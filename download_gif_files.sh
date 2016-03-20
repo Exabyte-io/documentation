46a2f6348c1f8946967a48b8ebe4506cb2fcee24
Timur Bazhirov <timur.bazhirov@gmail.com>
2016-03-20 00:29:58 -0700

    Adjusted port for docs app

--- download_gif_files.sh
+++
@@ -1,7 +0,0 @@
-mkdir tmp
-cd tmp
-wget http://docs.exabyte.io:81/files/public-documentation-gif.tar.gz
-tar -xzvf public-documentation-gif.tar.gz
-mv *.gif ../../docs/tutorials/images
-cd -
-rm -rf tmp
