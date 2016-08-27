78e43f566d321010a26659eee5e8edf384b32238
Derry Birkett <me@derrybirkett.com>
2016-08-27 14:14:47 -0700

    Introduce nav collapse
    Added js and css for expanding the first subnav of a subnav.

---
+++ exabyte_theme/extra.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $('ul.subnav > ul.subnav').on("click", function() {
+        console.log("Clicked");
+        $(this).toggleClass("active");
+        $(this).next('ul.subnav').slideToggle(200).toggleClass("active");
+    })
+});
