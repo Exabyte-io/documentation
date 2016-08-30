a487dd65ba0cfa0c994c3b0b7118d07f1feefe59
Derry Birkett <me@derrybirkett.com>
2016-08-30 16:56:01 -0700

    Update nav collapse to keep current page open and tweak styling

--- exabyte_theme/extra.js
+++ exabyte_theme/extra.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
-    $('ul.subnav').on("click", function() {
+    $('ul.subnav').on("click", function(event) {
+        //$(".toctree-l1").hide(90); // Close other menus
         $(this).toggleClass("active");
-        $(".toctree-l1", this).slideToggle(200);
+        $(".toctree-l1", this).show(200);
     })
+
+    $('li.current').show(50);
+    $('li.current').siblings().show(0);
 });
