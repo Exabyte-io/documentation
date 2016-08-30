8432f6834ba71c4bb8dea12ceb0d65230390957e
Derry Birkett <me@derrybirkett.com>
2016-08-30 12:56:27 -0700

    Collapsible nav

--- exabyte_theme/extra.js
+++ exabyte_theme/extra.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $('ul.subnav > ul.subnav').on("click", function() {
+    $('ul.subnav').on("click", function() {
         console.log("Clicked");
         $(this).toggleClass("active");
         $(this).next('ul.subnav').slideToggle(200).toggleClass("active");
