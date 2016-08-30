7a8a7d7544ae30543f0cc8ce1d8e54d902e6f1ab
Derry Birkett <me@derrybirkett.com>
2016-08-30 13:00:41 -0700

    Add sliding animation

--- exabyte_theme/extra.js
+++ exabyte_theme/extra.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
     $('ul.subnav').on("click", function() {
-        console.log("Clicked");
         $(this).toggleClass("active");
-        $(this).next('ul.subnav').slideToggle(200).toggleClass("active");
+        $(".toctree-l1", this).slideToggle(200);
     })
 });
