a8e17ef8c93e174404c0bc1b46d5e164cf759edd
Gabriele Mogni <gabriele@exabyte.io>
2019-03-28 12:03:31 +0100

    finished tag combinator script

--- docs/tags/tag-combinator.py
+++ docs/tags/tag-combinator.py
@@ -3,7 +3,7 @@ import os
 
 # Enter keywords associated with tag file of interest, and present within its pathname. All common files in the way will also be added.
 # For example, to retrieve the tags associated with bandstructure calculations listed in bandstructure.md file one would insert the following keywords: dft, electronic, bandstructure.
-# ALL entered keywords have to be contained within pathname of file of interest
+# ALL entered keywords have to be contained within pathname of file of interest, and point to its exact location
 
 list_keywords=raw_input('enter keywords for video separated by commas: ').split(',')
 for i in range(len(list_keywords)):
@@ -45,7 +45,7 @@ for i in range(len(list_files)):
     list_path= list_files[i].split('/')
     for j in range(len(list_path)):
         list_path[j]= list_path[j].replace('.md','')
-    if [list_path[j] in list_keywords  for j in range(len(list_path))] == [True]*len(list_keywords):
+    if [list_path[j] in list_keywords  for j in range(len(list_path))].count(True) >= len(list_keywords):
         common_file = open(list_files[i])
         list_lines = common_file.readlines()
         for k in range(len(list_lines)):
