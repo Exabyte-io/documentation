f3bef59b0b16c8219fe6530d6103ffe8df8a0720
Gabriele Mogni <gabriele@exabyte.io>
2019-04-02 18:38:17 +0200

    autoformat file with PEP8

--- docs/tags/tag-combinator.py
+++ docs/tags/tag-combinator.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 
-import os
 import json
+import os
 
-
-video_file=raw_input('Enter video file name in mp4 folder: ')
-title=raw_input('Enter video title: ')
+video_file = raw_input('Enter video file name in mp4 folder: ')
+title = raw_input('Enter video title: ')
 tags = raw_input('Enter JSON ID for tags list: ')
-
 
 with open('tags.json') as json_file:
     python_obj = (json.load(json_file))
 
+
 def retrieve_key(key):
-  for i in range(len(python_obj)):
-    if key in python_obj[i]:
-       return python_obj[i][key]
+    for i in range(len(python_obj)):
+        if key in python_obj[i]:
+            return python_obj[i][key]
 
 
-tags_list= retrieve_key('@'+tags)
+tags_list = retrieve_key('@' + tags)
 
-tags_list_1=[]
+tags_list_1 = []
 for i in range(len(tags_list)):
-    if tags_list[i][0]=="@":
+    if tags_list[i][0] == "@":
         tags_list_1.extend(retrieve_key(tags_list[i]))
 
-tags_list=tags_list_1+tags_list
-tags_list=[tags_list[i] for i in range(len(tags_list)) if '@' not in tags_list[i]]
+tags_list = tags_list_1 + tags_list
+tags_list = [tags_list[i] for i in range(len(tags_list)) if '@' not in tags_list[i]]
 
-os.system('youtube-upload --title="'+title+'"  --description="A.S.Mutter plays Beethoven"   --category="Science & Technology"  --tags="'+ ",".join(
-    tags_list)+'"  --embeddable=True --privacy unlisted ../../scripts/mp4/'+video_file)
+os.system(
+    'youtube-upload --title="' + title + '"  --description="A.S.Mutter plays Beethoven"   --category="Science & Technology"  --tags="' + ",".join(
+        tags_list) + '"  --embeddable=True --privacy unlisted ../../scripts/mp4/' + video_file)
