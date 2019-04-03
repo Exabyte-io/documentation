c35c682331f362f338f74d7f98e59086fe9a98b8
Gabriele Mogni <gabriele@exabyte.io>
2019-04-03 20:14:00 +0200

    created folders for tag files

--- docs/tags/tag-combinator.py
+++ docs/tags/tag-combinator.py
@@ -3,29 +3,38 @@
 import json
 import os
 
-video_file = raw_input('Enter video file name in mp4 folder: ')
-title = raw_input('Enter video title: ')
-tags = raw_input('Enter JSON ID for tags list: ')
+#video_file = raw_input('Enter video file name in mp4 folder: ')
+#title = raw_input('Enter video title: ')
+#tags = raw_input('Enter JSON ID for tags list: ')
 
-
-def retrieve_key(key):
-    key = key[1:]
-    with open(key + '.json') as json_file:
+with open('tutorials-metadata/kpt-convergence.json') as json_file:
         python_obj = (json.load(json_file))
-    return python_obj[0]['@' + key]
 
 
-tags_list = retrieve_key('@' + tags)
+print python_obj
 
-tags_list_1 = []
-for i in range(len(tags_list)):
-    if tags_list[i][0] == "@":
-        tags_list_1.extend(retrieve_key(tags_list[i]))
 
-tags_list = tags_list_1 + tags_list
-tags_list = [tags_list[i] for i in range(len(tags_list)) if '@' not in tags_list[i]]
-print tags_list
 
-os.system(
-    'youtube-upload --title="' + title + '"  --description="A.S.Mutter plays Beethoven"   --category="Science & Technology"  --tags="' + ",".join(
-        tags_list) + '"  --embeddable=True --privacy unlisted ../../scripts/mp4/' + video_file)
+
+
+#def retrieve_key(key):
+#    key = key[1:]
+#    with open(key + '.json') as json_file:
+#        python_obj = (json.load(json_file))
+#    return python_obj[0]['@' + key]
+
+
+#tags_list = retrieve_key('@' + tags)
+
+#tags_list_1 = []
+#for i in range(len(tags_list)):
+#    if tags_list[i][0] == "@":
+#        tags_list_1.extend(retrieve_key(tags_list[i]))
+
+#tags_list = tags_list_1 + tags_list
+#tags_list = [tags_list[i] for i in range(len(tags_list)) if '@' not in tags_list[i]]
+#print tags_list
+
+#os.system(
+#    'youtube-upload --title="' + title + '"  --description="A.S.Mutter plays Beethoven"   --category="Science & Technology"  --tags="' + ",".join(
+#        tags_list) + '"  --embeddable=True --privacy unlisted ../../scripts/mp4/' + video_file)
