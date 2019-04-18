d4e2f094e486c4546a4c5f3951139c6a4494d53c
Mohammad Mohammadi <mohammad@exabyte.io>
2019-04-18 13:29:48 -0700

    Clean up scripts

--- scripts/download_gif_files.sh
+++
@@ -1,7 +0,0 @@
-mkdir tmp
-cd tmp
-wget http://docs.exabyte.io/files/public-documentation-gif.tar.gz
-tar -xzvf public-documentation-gif.tar.gz
-mv *.gif ../../docs/tutorials/images
-cd -
-rmdir -rf tmp
