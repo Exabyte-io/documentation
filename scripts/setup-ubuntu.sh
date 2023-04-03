cab8cbbad427818239cdc59b398838036979ac54
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-04-03 00:32:18 +0000

    use ubuntu 22.04 with python 3.9

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+# requires prior installation of python 3.7 || 3.8 || 3.9 and pip
 sudo apt update
 sudo apt install -y --no-install-recommends \
     curl \
     git \
-    git-lfs \
-    python3 \
-    python3-pip
-pip install virtualenv
+    git-lfs
 git lfs pull
+git submodule update --recursive --init
 if [ -f requirements.txt ]; then
     pip install -r requirements.txt
 fi
-git submodule update --recursive --init
